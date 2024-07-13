c9be611861800b70b241f478c28f107fc651f26b
Belek Abdy <beshik2013@yandex.ru>
2024-07-13 13:03:26 +0300

    add new files

---
+++ Homeworks/Function/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿Задача 1: Напишите программу, которая бесконечно запрашивает 
+целые числа с консоли. Программа завершается при вводе символа 
+‘q’ или при вводе числа, сумма цифр которого чётная.
+
+while (true)
+{
+    Console.Write("Введите целое число или 'q' для выхода: ");
+    string input = Console.ReadLine();
+        
+ if (input.ToLower() == "q")
+     {
+         break;
+     }
+            
+ if (int.TryParse(input, out int number))
+    {
+      int sum = 0;
+      while (number > 0)
+    {
+     sum += number % 10;
+     number /= 10;
+    }
+ if (sum % 2 == 0)
+    {
+       Console.WriteLine("Сумма цифр введенного числа четная. Программа завершена.");
+       break;
+    }
+    }
+     else
+     {
+        Console.WriteLine("Введено не число. Попробуйте снова.");
+    }
+}
+
+
+
+
+Задача 2: Задайте массив заполненный случайными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Random random = new Random();
+int[] numbers = new int[10];
+int evenCount = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(100, 1000);
+    System.Console.Write(numbers[i] + " ");
+    if(numbers[i] % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+ System.Console.WriteLine($"\nКоличество чётных чисел в массиве: {evenCount}");
+
+
+
+
+Задача 3: Напишите программу, которая перевернёт одномерный массив
+(первый элемент станет последним, второй – предпоследним и т.д.)
+
+int[] Array = { 1, 2, 3, 4, 5 };
+int[] reversedArray = new int[Array.Length];
+
+for (int i = 0; i < Array.Length; i++)
+    {
+        reversedArray[i] = Array[Array.Length - 1 - i];
+    }
+Console.WriteLine("Перевернутый массив:");
+ foreach (int element in reversedArray)
+    {
+        Console.Write(element + " ");
+    }
---
+++ Homeworks/Program.cs
@@ -0,0 +1,90 @@
+﻿// Урок 2. Простые Алгоритмы
+// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+bool multiplicity (int x)
+{
+    if (x % 7 == 0 && x % 23 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+void Result(int x)
+{
+    if(multiplicity (x)) 
+    {
+        System.Console.WriteLine("да");
+    }
+    else
+    {
+        System.Console.WriteLine("нет");
+    }
+}
+Console.WriteLine("Input number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Result (x);
+
+
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+System.Console.WriteLine("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x != 0 && y != 0)
+{
+if (x > 0 && y > 0)
+    {
+     Console.WriteLine("1");
+    }
+else if (x < 0 && y > 0)
+    {
+    Console.WriteLine("2");
+    }
+else if (x < 0 && y < 0)
+    {
+    Console.WriteLine("3");
+    }
+else 
+    {
+    Console.WriteLine("4");
+    }
+    }
+
+// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.WriteLine("Введите целое число от 10 до 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10 && number <= 99)
+{
+ int firstDigit = number / 10; 
+ int secondDigit = number % 10; 
+
+if (firstDigit >= secondDigit)
+  {
+   System.Console.WriteLine("=> " + firstDigit);
+  }
+else
+  {
+   System.Console.WriteLine("=> " + secondDigit);
+  }
+}
+
+
+// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string number = N.ToString();
+
+for (int i = 0; i < number.Length - 1; i++)
+ {
+  System.Console.Write(number[i] + ", ");
+ }
+ {
+  System.Console.WriteLine(number[number.Length - 1]);
+ }
