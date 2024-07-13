6c42ac17838fe0225bc2cefa1ec56a6d7af81305
Belek Abdy <beshik2013@yandex.ru>
2024-07-13 18:13:00 +0300

    add HW:Task4

---
+++ Homeworks/Массивы и строки/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Задайте строку, состоящую из слов, разделенных пробелами.
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+Console.WriteLine("Введите строку из слов:");
+string input = Console.ReadLine();
+
+string reversed = ReverseWords(input);
+Console.WriteLine("Строка с обратным порядком слов:");
+Console.WriteLine(reversed);
+
+string ReverseWords(string str)
+{
+    string[] words = str.Split(' ');
+    Array.Reverse(words);
+    return string.Join(" ", words);
+}
+
+
+
