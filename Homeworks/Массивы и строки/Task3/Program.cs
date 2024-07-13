de33652f2c588623364343bb891aaab9ce815616
Belek Abdy <beshik2013@yandex.ru>
2024-07-13 17:14:53 +0300

    add HW:Task3

---
+++ Homeworks/Массивы и строки/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3: Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+
+Console.WriteLine("Введите строку:");
+string input = Console.ReadLine();
+
+if (IsPalindrome(input))
+{
+    Console.WriteLine("Строка является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Строка не является палиндромом.");
+}
+
+static bool IsPalindrome(string str)
+{
+    int left = 0;
+    int right = str.Length - 1;
+
+    while (left < right)
+    {
+        if (char.ToLower(str[left]) != char.ToLower(str[right]))
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
