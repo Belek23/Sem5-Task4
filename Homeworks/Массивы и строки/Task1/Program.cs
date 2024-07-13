6957da1309195d7dedeedfa4cb786e3031f3376a
Belek Abdy <beshik2013@yandex.ru>
2024-07-13 16:39:05 +0300

    add HW:Task1

---
+++ Homeworks/Массивы и строки/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+char[,] array = { { 's', 't', 'r' }, { 'i', 'n', 'g' } };
+string result = string.Empty;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        result += array[i, j];
+    }
+    System.Console.WriteLine(result);
+}
